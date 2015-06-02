fdd88d5ad9965ee2833d9a88638a9cf65857aa99
Finlay <fsm@st-andrews.ac.uk>
2015-06-02 16:20:05 +0100

    small tidy up, now should focus on making the android part of it

--- app/src/main/java/com/example/fsm/manogotchi/Main.java
+++ app/src/main/java/com/example/fsm/manogotchi/Main.java
@@ -1,15 +1,16 @@
-//package com.example.fsm.manogotchi;
+package com.example.fsm.manogotchi;
 
 /**
  * Created by fsm on 02/06/15.
+ * used for testing Person.java without android
  */
 public class Main {
     public static void main(String[] args){
         Person jim = new Person();
         jim.print();
         while (jim.runHour() != -1){
-            if (jim.getAge() % 24 ==0)
             jim.print();
+            System.out.println();
         }
         System.out.println("Jim died!");
         jim.print();
